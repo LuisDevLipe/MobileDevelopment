8f58f44da3efe7ca823676f50070960fb8b550f5
LuisDevLipe <luisfelipebsj@gmail.com>
2025-05-31 21:25:32 -0300

    Added home.page to the auth protected routes

--- Entrega-final/entrega-final/src/app/app.routes.ts
+++ Entrega-final/entrega-final/src/app/app.routes.ts
@@ -7,6 +7,7 @@ export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    canActivate: [authGuard], // Use the authGuard to protect the home route
   },
   {
     path: '',
