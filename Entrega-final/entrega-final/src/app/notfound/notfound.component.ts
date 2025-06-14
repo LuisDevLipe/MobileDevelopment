b45471fabd91ff13e4c3511fa634c93329c3e16a
LuisDevLipe <luisfelipebsj@gmail.com>
2025-06-14 03:28:54 -0300

    Fixing: Layout not found page

--- Entrega-final/entrega-final/src/app/notfound/notfound.component.ts
+++ Entrega-final/entrega-final/src/app/notfound/notfound.component.ts
@@ -1,14 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  IonTitle,
+  IonToolbar,
+  IonHeader,
+  IonBackButton,
+  IonContent,
+  IonButtons,
+  IonItem,
+  IonLabel,
+  IonButton,
+  IonIcon,
+  IonRippleEffect,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-notfound',
   templateUrl: './notfound.component.html',
   styleUrls: ['./notfound.component.scss'],
+  standalone: true,
+  imports: [
+    IonRippleEffect,
+    IonIcon,
+    IonButton,
+    IonLabel,
+    IonItem,
+    IonButtons,
+    IonContent,
+    IonBackButton,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+  ],
 })
-export class NotfoundComponent  implements OnInit {
-
-  constructor() { }
+export class NotfoundComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
-
 }
