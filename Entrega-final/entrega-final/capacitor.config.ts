35fb310f8f910ef592fe11c347b78bba4726e1da
LuisDevLipe <luisfelipebsj@gmail.com>
2025-06-20 00:05:20 -0300

    Added: Android icons and a release apk for testing.

--- Entrega-final/entrega-final/capacitor.config.ts
+++ Entrega-final/entrega-final/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.g2l2.entregafinal',
   appName: 'entrega-final.2',
   webDir: 'www',
   plugins: {
